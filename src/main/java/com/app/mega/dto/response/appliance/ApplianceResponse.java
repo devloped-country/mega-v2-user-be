7469ad24229f39b2de835a644a0c3a5a008efa70
CDDcookie <wlsdnxwlsdn@gamil.com>
2023-12-15 13:22:50 +0900

    fix:API수정완료

--- src/main/java/com/app/mega/dto/response/appliance/ApplianceResponse.java
+++ src/main/java/com/app/mega/dto/response/appliance/ApplianceResponse.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class ApplianceResponse {
     private Integer status;
     private Long attendance;
-    private LocalDate time;//dto
+    private LocalDateTime time;//dto
     private Long userId;
     private String reason;
 }
