e1e848535ee5bbd554092157a13b4d2a5469969d
eeheg <sjh8924@naver.com>
2023-12-18 03:53:57 +0900

    아

--- src/main/java/com/app/mega/entity/Attendance.java
+++ src/main/java/com/app/mega/entity/Attendance.java
@@ -1,12 +1,8 @@
 package com.app.mega.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.*;
@@ -23,6 +19,9 @@ public class Attendance {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = true)
+  private LocalDate attendanceDate;
 
   private LocalDateTime startTime;
   private LocalDateTime endTime;
